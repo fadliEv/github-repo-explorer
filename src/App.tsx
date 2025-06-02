2a778f502c910a1f3f71e14b6cd930b561e56c2c
fadli <fadlirahmanfauzan04@gmail.com>
2025-06-02 13:34:03 +0700

    init app entry point

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-function App() {
-  return (
-    <>
-     <h1 className="text-3xl font-bold underline">
-        Hello world!
-    </h1>
-    </>
-  )
-}
+import React, { useState } from 'react';
+import InputUsername from './components/InputUsername';
 
-export default App
+const App: React.FC = () => {  
+
+  const handleSearch = (username: string) => {};
+
+  return (
+    <div className="min-h-screen bg-gray-50 p-8">
+      <h1 className="text-3xl font-bold mb-6 text-center">GitHub User Search</h1>
+      <InputUsername onSearch={handleSearch} />      
+    </div>
+  );
+};
+
+export default App;

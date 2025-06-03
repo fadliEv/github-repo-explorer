e4ed5d298fb7cf349902fbe2b13fb212ea52d937
fadli <fadlirahmanfauzan04@gmail.com>
2025-06-03 19:34:34 +0700

    fix : rename homepage

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/test-deploy-react/',
+  base: '/github-repo-explorer/',
   plugins: [
     react(),
     tailwindcss()

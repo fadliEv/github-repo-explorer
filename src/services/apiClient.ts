80e0165e7f48e12cfa112c564c0f3658921dc2f5
fadli <fadlirahmanfauzan04@gmail.com>
2025-06-03 17:54:37 +0700

    fix : remove unused code import

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -4,8 +4,7 @@ const apiClient = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 8000,
   headers: {
-    Accept: 'application/vnd.github.v3+json',
-    Authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`,
+    Accept: 'application/vnd.github.v3+json',    
   },
 });
 
